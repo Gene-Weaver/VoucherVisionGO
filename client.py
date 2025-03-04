bc5d55fa927867c8b219dcfd0f2cf77cc5f01a07
Gene-Weaver <willwe@umich.edu>
2025-03-03 22:05:15 -0500

    json encoder

--- client.py
+++ client.py
@@ -64,7 +64,7 @@ def process_image(server_url, image_path, engines=None, prompt=None):
         # Check if the request was successful
         if response.status_code == 200:
             print("Raw response from server:")
-            print(response.text[:1000]) 
+            print(response.text) 
             return json.loads(response.text, object_pairs_hook=OrderedDict)
         else:
             error_msg = f"Error: {response.status_code}"
