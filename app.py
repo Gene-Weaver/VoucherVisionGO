ca624e0b106054223572c56f3e5753980647009a
Gene-Weaver <willwe@umich.edu>
2025-03-10 23:54:13 -0400

    api usage guides

--- app.py
+++ app.py
@@ -1215,6 +1215,9 @@ def auth_success():
                 }
             });
             </script>
+        </div>
+        
+        <script>
           // Firebase configuration
           const firebaseConfig = {
             apiKey: "{{ api_key }}",
@@ -1641,7 +1644,7 @@ def signup_page():
         <title>VoucherVision Signup</title>
         <script src="https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js"></script>
         <script src="https://www.gstatic.com/firebasejs/10.0.0/firebase-auth-compat.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
         <style>
           /* Existing styles remain the same */
